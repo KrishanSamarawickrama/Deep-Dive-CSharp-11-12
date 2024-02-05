28e0d03972891583044cff3951570d3f7362384c
Krishan Samarawickrama <Krishans1990@live.com>
2024-02-05 11:59:55 +0530

    Create StaticAbstractMembersInInterfaces.cs

---
+++ StaticAbstractMembersInInterfaces.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Deep_Dive_CSharp_11_12;
+internal class StaticAbstractMembersInInterfaces
+{
+}

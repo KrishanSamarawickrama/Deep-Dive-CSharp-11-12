769efea7ea679436b9daec18fcfe2cd344fbf4ad
Krishan Samarawickrama <Krishans1990@live.com>
2024-02-05 12:00:02 +0530

    Update StaticAbstractMembersInInterfaces.cs

--- StaticAbstractMembersInInterfaces.cs
+++ StaticAbstractMembersInInterfaces.cs
@@ -5,6 +5,30 @@ using System.Text;
 using System.Threading.Tasks;
 
 namespace Deep_Dive_CSharp_11_12;
-internal class StaticAbstractMembersInInterfaces
+internal static class StaticAbstractMembersInInterfaces
 {
+    //Before
+    public interface IMath<T>
+    {
+        T Add(T a, T b);
+        // Static method not allowed here
+    }
+
+    public class IntMath : IMath<int>
+    {
+        public int Add(int a, int b) => a + b;
+        public static int StaticAdd(int a, int b) => a + b;
+    }
+
+
+    //After
+    public interface IMath12<T>
+    {
+        static abstract T Add(T a, T b);
+    }
+
+    public class IntMath12 : IMath12<int>
+    {
+        public static int Add(int a, int b) => a + b;
+    }
 }
